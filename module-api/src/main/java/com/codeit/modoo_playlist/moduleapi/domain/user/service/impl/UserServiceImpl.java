22f906c6336044ed3ac06be75b71d61213f74765
Hize18 <98449664+Hize18@users.noreply.github.com>
2026-09-09 17:18:18 +0900

    feat: 서비스 기초 구현
    현재 진행중인 회원가입 부분에 대해서 구현을 먼저 하고 나머지 부분은 진행하면서 수정할 예정

--- module-api/src/main/java/com/codeit/modoo_playlist/moduleapi/domain/user/service/impl/UserServiceImpl.java
+++ module-api/src/main/java/com/codeit/modoo_playlist/moduleapi/domain/user/service/impl/UserServiceImpl.java
@@ -1,13 +1,58 @@
 package com.codeit.modoo_playlist.moduleapi.domain.user.service.impl;
 
+import com.codeit.modoo_playlist.core.domain.user.entity.User;
 import com.codeit.modoo_playlist.moduleapi.domain.user.repository.UserRepository;
 import com.codeit.modoo_playlist.moduleapi.domain.user.service.UserService;
+import com.codeit.modoo_playlist.moduleapi.dto.UserDto;
+import com.codeit.modoo_playlist.moduleapi.dto.request.UserCreateRequest;
+import com.codeit.modoo_playlist.moduleapi.dto.request.UserProfileUpdateRequest;
+import com.codeit.modoo_playlist.moduleapi.mapper.UserMapper;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
   private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+  private final UserMapper userMapper;
+
+  @Transactional
+  @Override
+  public UserDto create(UserCreateRequest request) {
+    throw new UnsupportedOperationException("구현 예정");
+  }
+
+  @Transactional(readOnly = true)
+  @Override
+  public UserDto getUser(UUID userId) {
+    throw new UnsupportedOperationException("구현 예정");
+  }
+
+  @Transactional
+  @Override
+  public UserDto updateUser(
+      UUID userId,
+      UserProfileUpdateRequest request,
+      MultipartFile image
+  ) {
+//    본인 수정 권한 검증.
+
+    User user = userRepository.findById(userId).orElse(null);
+//    TODO: UserNotFound 예외 정의 후 수정(orElse -> orElseThrow)
+
+    String imageUrl = null;
+    if (image != null && !image.isEmpty()) {
+      imageUrl = image.getOriginalFilename();
+//      TODO: 이미지 저장 후 URL로 받아오는 메서드로 변경
+    }
+
+    user.updateProfile(request.name(), imageUrl);
+    return userMapper.toDto(user);
+  }
 }
