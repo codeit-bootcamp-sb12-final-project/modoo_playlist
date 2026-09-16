2260967581db983c6768c274256907a60f2806fa
Hize18 <98449664+Hize18@users.noreply.github.com>
2026-09-16 20:39:11 +0900

    fix: QueryDsl EM 수정

--- infra/src/main/java/com/codeit/modoo_playlist/infra/config/QuerydslConfig.java
+++ infra/src/main/java/com/codeit/modoo_playlist/infra/config/QuerydslConfig.java
@@ -2,13 +2,18 @@ package com.codeit.modoo_playlist.infra.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QuerydslConfig {
-    @Bean
-    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
-        return new JPAQueryFactory(em);
-    }
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Bean
+  public JPAQueryFactory jpaQueryFactory() {
+    return new JPAQueryFactory(entityManager);
+  }
 }
