0fbc14f53e649aba734ea4d10d05b6f5a9d2f7cb
Woojin-Jeong <dnwls3958@naver.com>
2026-09-21 18:05:54 +0900

    fix: 사용 안하는 import 제거

--- module-api/src/main/java/com/codeit/modoo_playlist/moduleapi/domain/watchingsession/service/WatchingSessionService.java
+++ module-api/src/main/java/com/codeit/modoo_playlist/moduleapi/domain/watchingsession/service/WatchingSessionService.java
@@ -8,7 +8,6 @@ import com.codeit.modoo_playlist.core.global.exception.ErrorCode;
 import com.codeit.modoo_playlist.infra.mapper.ContentSummaryMapper;
 import com.codeit.modoo_playlist.moduleapi.domain.content.repository.jpa.ContentRepository;
 import com.codeit.modoo_playlist.moduleapi.domain.content.repository.jpa.ContentTagRepository;
-import com.codeit.modoo_playlist.infra.event.kafka.WatchingSessionStartedEvent;
 import com.codeit.modoo_playlist.moduleapi.domain.user.repository.UserRepository;
 import com.codeit.modoo_playlist.moduleapi.domain.watchingsession.repository.ApiWatchingSessionRepository;
 import com.codeit.modoo_playlist.core.domain.content.dto.ContentSummaryResponse;
@@ -17,7 +16,6 @@ import com.codeit.modoo_playlist.core.global.common.dto.base.SliceCursorRequest;
 import com.codeit.modoo_playlist.core.domain.watchingSession.dto.CursorResponseWatchingSessionDto;
 import com.codeit.modoo_playlist.infra.mapper.WatchingSessionMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,8 +29,6 @@ public class WatchingSessionService {
     private final UserRepository userRepository;
     private final ContentRepository contentRepository;
     private final ContentTagRepository contentTagRepository;
-    private final ApplicationEventPublisher eventPublisher;
-
 
     private final WatchingSessionMapper watchingSessionMapper;
     private final ContentSummaryMapper contentSummaryMapper;
